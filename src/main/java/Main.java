e2fbb79c96aea3af63d4c4378bdc072392dcb996
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-11-11 16:03:09 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int tuition = 10000 ;
+        int tuitionIncrease = tuition + 500;
+        int TuitionIncreaseYearTwo=  tuition + 1000 ;
+        int TuitionIncreaseYearTen = tuition * 10 + 5000 ;
+
+      
+          
+          System.out.println("Tuition this year = " + tuitionIncrease);
+          System.out.println("Tuition next year = " + TuitionIncreaseYearTwo);
+          System.out.println("Tuition in ten years = " + TuitionIncreaseYearTen);
+        }
+    }
+
+    // Uncomment the below code if JUnit testing is required
+    // @Test
+    // void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+
